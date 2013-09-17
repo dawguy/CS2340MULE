1cdd45fb841803d96e2a2ffa5ba16102ef9a7cce
dawguy <wright.david.27@gmail.com>
2013-09-16 20:11:33 -0400

    Started to work on the project.

---
+++ MuleGame/src/screens/SettingsScreen.java
@@ -0,0 +1,52 @@
+package screens;
+
+import com.badlogic.gdx.Screen;
+import com.me.mygdxgame.Mule;
+
+public class SettingsScreen implements Screen{
+	public SettingsScreen(Mule mule){
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+}
